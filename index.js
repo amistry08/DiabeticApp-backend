a12d779cf5d1adffbffa807d6dc80f27a70a0c18
amistry08 <ayushmistry1998@gmail.com>
2023-05-25 11:54:12 -0400

    Connection to mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const http = require("http");
+require("dotenv").config();
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const mongoose = require("mongoose");
+const http = require("http");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+mongoose.set("strictQuery", false);
+
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+const uri = process.env.URI;
+const db_username = process.env.USERNAME;
+const db_password = process.env.PASSWORD;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -11,4 +20,25 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+mongoose.connect(
+  `mongodb+srv://${db_username}:${[
+    db_password,
+  ]}@diabeticapp-cluster.qbmzh8t.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+
+db.once("open", function () {
+  console.log("MongoDB connected successfully");
+  app.listen(3001, () => {
+    console.log("Listening on port 3001");
+  });
 });
